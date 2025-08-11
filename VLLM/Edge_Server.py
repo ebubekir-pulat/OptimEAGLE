d3fea8f05608a2e445ebb615f88227f9e444c7f6
Ebubekir <ebubekirpulat15@gmail.com>
2025-08-11 21:33:01 +0930

    update Edge Server

--- VLLM/Edge_Server.py
+++ VLLM/Edge_Server.py
@@ -1,32 +1,45 @@
 from vllm import LLM, SamplingParams
 import pandas as pd
 import time
+import torch
+
+VLLM_USE_V1=1
 
 # Getting Spec-Bench Questions
 # Below line from: https://stackoverflow.com/questions/50475635/loading-jsonl-file-as-json-objects
 jsonObj = pd.read_json(path_or_buf='../question.jsonl', lines=True)
 sb_prompts = [jsonObj.at[i, 'turns'] for i in range(len(jsonObj))]
+questions = []
+
+for question in sb_prompts:
+    questions.append(question)
 
 # Code in Rest of File From: https://docs.vllm.ai/en/stable/features/spec_decode.html#speculating-using-eagle-based-draft-models
-sampling_params = SamplingParams(temperature=0.0, max_new_tokens=256)
+sampling_params = SamplingParams(temperature=0.0)
 
 llm = LLM(
     model="meta-llama/Llama-3.1-8B-Instruct",
-    tensor_parallel_size=4,
+    tensor_parallel_size=1,
     speculative_config={
+        # Reference for below code line: https://github.com/vllm-project/vllm/issues/21447
+        "method": "eagle3",
         "model": "yuhuili/EAGLE3-LLaMA3.1-Instruct-8B",
         "draft_tensor_parallel_size": 1,
         "num_speculative_tokens": 5,
+        "attn_implementation": "flash_attention_2"
     },
 )
 
 start = time.perf_counter_ns()
 
-outputs = llm.generate(sb_prompts, sampling_params)
+for question in questions:
+    outputs = llm.generate(question, sampling_params)
 
 finish = time.perf_counter_ns()
 elapsed = finish - start
 print("Wall Time (ns): ", elapsed)
+
+torch.distributed.destroy_process_group()
 
 #for output in outputs:
 #    prompt = output.prompt
