090c2709bebd6dfe39a756ac8ed5947d42fb66a2
Ebubekir <ebubekirpulat15@gmail.com>
2025-08-17 23:24:58 +0930

    clean up

--- FlashEAGLE/OutputSimilarity.py
+++ FlashEAGLE/OutputSimilarity.py
@@ -1,23 +1,31 @@
 # pip install -U FlagEmbedding
 
 from FlagEmbedding import BGEM3FlagModel
+import numpy as np
 
 # Below Code From: https://huggingface.co/BAAI/bge-m3
 
-eagle_answers = ["What is BGE M3?", "Defination of BM25"]
-approx_answers = ["BGE M3 is an embedding model supporting dense retrieval, lexical matching and multi-vector interaction.", 
-               "BM25 is a bag-of-words retrieval function that ranks a set of documents based on the query terms appearing in each document"]
+if __name__ == '__main__':
+    compresseagle_answers = []
+    flasheagle_answers = []
+    similarity_scores = []
 
-model = BGEM3FlagModel('BAAI/bge-m3',  
-                       use_fp16=True)
+    model = BGEM3FlagModel('BAAI/bge-m3',  
+                        use_fp16=True)
+    
+    for i in range(len(compresseagle_answers)):
+        embeddings_1 = model.encode(compresseagle_answers[i])['dense_vecs']
+        embeddings_2 = model.encode(flasheagle_answers[i])['dense_vecs']
+        similarity = embeddings_1 @ embeddings_2.T
+        similarity_scores.append(similarity)
 
-embeddings_1 = model.encode(eagle_answers, 
-                            batch_size=12, 
-                            max_length=4096,
-                            )['dense_vecs']
-embeddings_2 = model.encode(approx_answers)['dense_vecs']
-similarity = embeddings_1 @ embeddings_2.T
-print(similarity)
-
+    print("Mean Similarity Score: ", np.mean(similarity_scores))
 
 # Note: Put Reference For Above Link
+
+'''
+References
+
+1.
+
+'''
