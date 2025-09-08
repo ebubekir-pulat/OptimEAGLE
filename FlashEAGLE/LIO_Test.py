7f37bfa2562e5bceba38aafbdb935d20787d2721
Ebubekir <ebubekirpulat15@gmail.com>
2025-09-08 12:02:30 +0930

    LIO Ready

--- FlashEAGLE/LIO_Test.py
+++ FlashEAGLE/LIO_Test.py
@@ -81,7 +81,7 @@ client = openai.Client(base_url=f"http://127.0.0.1:{port}/v1", api_key="None")
 
 
 LIO_outputs = []
-summarise = True
+summarise = False
 ranked_retrieve = False
 test_runs = 1
 max_new_tokens = 128
@@ -94,7 +94,7 @@ print("Temperature: ", temp)
 print("Summarise: ", summarise, "\n")
 print("Ranked Retrieve: ", ranked_retrieve)
 
-# LongBench-E Assessment Loop
+# LIO Assessment Loop
 wall_times = []
 token_rates = []
 input_tokens = []
@@ -152,7 +152,7 @@ for test_run in range(test_runs):
         }
         LIO_outputs.append(output)
 
-# Print LongBench-E Results
+# Print LIO Results
 print(f"LIO Results for {LIO_model_paths[0]}:")
 print(f"Dataset: {dataset}")
 print(f"EAGLE Model: {EAGLE_model_paths[0]}")
