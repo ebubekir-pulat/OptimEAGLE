f22ce343527b69e956074f4e4f606884c753dfc4
Ebubekir <ebubekirpulat15@gmail.com>
2025-09-21 12:47:09 +0930

    Ready

--- FlashEAGLE/Context_Compress/AAI_AutoReg_Test.py
+++ FlashEAGLE/Context_Compress/AAI_AutoReg_Test.py
@@ -1,4 +1,6 @@
-# Context Compression Testing on Align-Anything-Instruction-100K-zh
+# Autoregressive Decoding, Context Compression Testing on Align-Anything-Instruction-100K-zh
+# Context Compression via Translation
+# Hyperparameters are "translate", "test_runs", "max_new_tokens"
 
 print("\n\n*******************************\nStarting AAI_AutoReg_Test.py\n\n")
 
@@ -24,6 +26,8 @@ def model_init():
 
 aai_ds = Data.aai_dataset()
 AAI_outputs = []
+
+# Hyperparameters
 translate = True
 test_runs = 1
 max_new_tokens = 128
@@ -78,7 +82,7 @@ for test_run in range(test_runs):
 
         # Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
         output = {
-            "id": hashlib.md5((question + Data.extract_response(aai_output)).encode()).hexdigest(),
+            "id": hashlib.md5((str(test_run) + question + Data.extract_response(aai_output)).encode()).hexdigest(),
             "output": Data.extract_response(aai_output)
         }
         AAI_outputs.append(output)
@@ -104,31 +108,34 @@ print("\n\n*******************************\nFinished Running AAI_AutoReg_Test.py
 '''
 References
 
-1. Y. Li, F. Wei, C. Zhang, and H. Zhang, “EAGLE: Speculative sampling requires rethinking feature
+1. DeepSeek-AI, “Deepseek-r1: Incentivizing reasoning capability in llms via reinforcement learning,” 2025.
+[Online]. Available: https://arxiv.org/abs/2501.12948
+
+2. Y. Li, F. Wei, C. Zhang, and H. Zhang, “EAGLE: Speculative sampling requires rethinking feature
 uncertainty,” in Proceedings of the 41st International Conference on Machine Learning, ser. Proceedings
 of Machine Learning Research, R. Salakhutdinov, Z. Kolter, K. Heller, A. Weller, N. Oliver, J. Scarlett,
 and F. Berkenkamp, Eds., vol. 235. PMLR, 21–27 Jul 2024, pp. 28 935–28 948. [Online]. Available:
 https://proceedings.mlr.press/v235/li24bt.html
 
-2. Y. Li, F. Wei, C. Zhang, and H. Zhang, “EAGLE-2: Faster inference of language models with dynamic
+3. Y. Li, F. Wei, C. Zhang, and H. Zhang, “EAGLE-2: Faster inference of language models with dynamic
 draft trees,” in Proceedings of the 2024 Conference on Empirical Methods in Natural Language Processing,
 Y. Al-Onaizan, M. Bansal, and Y.-N. Chen, Eds. Miami, Florida, USA: Association for Computational
 Linguistics, Nov. 2024, pp. 7421–7432. [Online]. Available: https://aclanthology.org/2024.emnlp-main.422/
 
-3. Y. Li, F. Wei, C. Zhang, and H. Zhang, “Eagle-3: Scaling up inference acceleration of large language models
+4. Y. Li, F. Wei, C. Zhang, and H. Zhang, “Eagle-3: Scaling up inference acceleration of large language models
 via training-time test,” 2025. [Online]. Available: https://arxiv.org/abs/2503.01840
 
-4. C. W. F. Y. S. S. Y. W. Y. Z. Y. H. H. Z. Y. Z. Shenggui Li, Yikai Zhu, “Specforge: Train speculative decoding
+5. C. W. F. Y. S. S. Y. W. Y. Z. Y. H. H. Z. Y. Z. Shenggui Li, Yikai Zhu, “Specforge: Train speculative decoding
 models effortlessly,” https://github.com/sgl-project/specforge, 2025.
 
-5. T. Wolf, L. Debut, V. Sanh, J. Chaumond, C. Delangue, A. Moi, P. Cistac, T. Rault, R. Louf, M. Funtowicz,
+6. T. Wolf, L. Debut, V. Sanh, J. Chaumond, C. Delangue, A. Moi, P. Cistac, T. Rault, R. Louf, M. Funtowicz,
 J. Davison, S. Shleifer, P. von Platen, C. Ma, Y. Jernite, J. Plu, C. Xu, T. L. Scao, S. Gugger,
 M. Drame, Q. Lhoest, and A. M. Rush, “Transformers: State-of-the-art natural language processing,”
 in Proceedings of the 2020 Conference on Empirical Methods in Natural Language Processing: System
 Demonstrations. Online: Association for Computational Linguistics, Oct. 2020, pp. 38–45. [Online]. Available:
 https://www.aclweb.org/anthology/2020.emnlp-demos.6
 
-6. L. Zheng, W.-L. Chiang, Y. Sheng, S. Zhuang, Z. Wu, Y. Zhuang, Z. Lin, Z. Li, D. Li, E. P. Xing, H. Zhang,
+7. L. Zheng, W.-L. Chiang, Y. Sheng, S. Zhuang, Z. Wu, Y. Zhuang, Z. Lin, Z. Li, D. Li, E. P. Xing, H. Zhang,
 J. E. Gonzalez, and I. Stoica, “Judging llm-as-a-judge with mt-bench and chatbot arena,” in Proceedings of
 the 37th International Conference on Neural Information Processing Systems, ser. NIPS ’23. Red Hook, NY,
 USA: Curran Associates Inc., 2023. 
