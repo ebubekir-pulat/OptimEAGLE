d4121317b51c9858d19277d85195e6807c209af0
Ebubekir <ebubekirpulat15@gmail.com>
2025-09-27 06:17:50 +0930

    Clean AAI

---
+++ FlashEAGLE/Context_Compress/AAI_Test.py
@@ -0,0 +1,185 @@
+# Context Compression Testing on Align-Anything-Instruction-100K-zh
+# Translation
+# Hyperparameters: eagle3, translate, test_runs, max_new_tokens, temp
+
+print("\n\n*******************************\nStarting AAI_Test.py\n\n")
+
+import time
+import numpy as np
+import json
+from sglang.test.doc_patch import launch_server_cmd
+from sglang.utils import wait_for_server, terminate_process
+import openai
+import Data
+import Compress
+import hashlib
+from matplotlib import pyplot as plt
+import sys
+
+def main(eagle3, translate):
+    base_model_paths = ["deepseek-ai/DeepSeek-R1-Distill-Llama-8B"]
+    EAGLE_model_paths = ["yuhuili/EAGLE3-DeepSeek-R1-Distill-LLaMA-8B"]
+
+    aai_ds = Data.aai_dataset()
+
+    # Hyperparameter
+    eagle3 = True
+
+    if eagle3 == True:
+        # Preparing SGLANG with EAGLE3
+        # Below Code Block From: https://docs.sglang.ai/advanced_features/speculative_decoding.html
+        server_process, port = launch_server_cmd(
+            f"""
+        python3 -m sglang.launch_server --model {base_model_paths[0]}  --speculative-algorithm EAGLE3 \
+            --speculative-draft-model-path {EAGLE_model_paths[0]} --dtype float16
+        """
+        )
+    else:
+        # Preparing AutoReg SGLANG
+        # Below Code Block From: https://docs.sglang.ai/advanced_features/speculative_decoding.html, https://docs.sglang.ai/basic_usage/send_request.html
+        server_process, port = launch_server_cmd(
+            f"""
+        python3 -m sglang.launch_server --model-path {base_model_paths[0]} --dtype float16
+        """
+        )
+
+
+    # Below Code Block From: https://docs.sglang.ai/advanced_features/speculative_decoding.html
+    wait_for_server(f"http://localhost:{port}")
+    client = openai.Client(base_url=f"http://127.0.0.1:{port}/v1", api_key="None")
+
+
+    AAI_outputs = []
+    # Hyperparameters
+    test_runs = 3
+    max_new_tokens = 2048
+    temp = 0.0
+
+    print("\nEvaluation Settings Chosen:")
+    print("EAGLE3: ", eagle3)
+    print("Translate: ", translate)
+    print("Test Runs: ", test_runs)
+    print("Max New Tokens: ", max_new_tokens)
+    print("Temperature: ", temp, "\n")
+
+    # AAI Assessment Loop
+    wall_times = []
+    token_rates = []
+    input_tokens = []
+    output_tokens = []
+
+    for test_run in range(test_runs):
+        run = 1
+        for prompt in aai_ds:
+            print("Test Run: ", test_run)
+            print("Test Question: ", run)
+            run += 1
+
+            if translate == True:
+                prompt = Compress.zh_to_en(prompt)[0]
+            
+            start = time.perf_counter_ns()
+
+            # Below Code Block From: https://docs.sglang.ai/advanced_features/speculative_decoding.html
+            response = client.chat.completions.create(
+                model=base_model_paths[0],
+                messages=[
+                    {"role": "user", "content": prompt},
+                ],
+                temperature=temp,
+                max_tokens=max_new_tokens,
+            )
+
+            finish = time.perf_counter_ns()
+
+            # Reference for below code line: https://stackoverflow.com/questions/77444332/openai-python-package-error-chatcompletion-object-is-not-subscriptable 
+            aai_output = response.choices[0].message.content
+
+            if translate == True:
+                aai_output = Compress.en_to_zh(aai_output)[0]
+            
+            elapsed = finish - start
+            wall_times.append(elapsed)
+
+            new_tokens = response.usage.completion_tokens
+            tokens_per_second = new_tokens / (elapsed * pow(10, -9))
+            token_rates.append(tokens_per_second)
+            output_tokens.append(new_tokens)
+
+            input_tokens.append(response.usage.prompt_tokens)
+
+            # Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
+            output = {
+                "id": hashlib.md5((str(test_run) + prompt + aai_output).encode()).hexdigest(),
+                "output": aai_output
+            }
+            AAI_outputs.append(output)
+
+    # Print AAI Results
+    if eagle3 == True:
+        print(f"AAI Results for {EAGLE_model_paths[0]}:")
+    else:
+        print(f"AAI Results for {base_model_paths[0]}:")
+    print("Mean Wall Time (ns): ", np.mean(wall_times))
+    print("Mean Tokens Generated/s: ", np.mean(token_rates))
+
+    # Below Code Line From: https://docs.sglang.ai/advanced_features/speculative_decoding.html
+    terminate_process(server_process)
+
+    translate_tag = ""
+
+    if translate == True:
+        translate_tag = "_Translate"
+
+    if eagle3 == True:
+        output_name = f"AAI_Output_EAGLE3_{EAGLE_model_paths[0].replace('/', '-')}{translate_tag}.jsonl" 
+    else:
+        output_name = f"AAI_Output_AutoReg_{base_model_paths[0].replace('/', '-')}{translate_tag}.jsonl" 
+
+    # Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
+    with open(output_name, "x") as f:
+        for output in AAI_outputs:
+            f.write(json.dumps(output) + "\n")
+
+
+    # Final Plots
+    plt.title("Input Tokens vs Token Rates")
+    plt.plot(input_tokens, token_rates)
+    plt.savefig("InputTokens_vs_TokenRates.png")
+
+    plt.title("Output Tokens vs Token Rates")
+    plt.plot(output_tokens, token_rates)
+    plt.savefig("OutputTokens_vs_TokenRates.png")
+
+
+    print("\n\n*******************************\nFinished Running AAI_Test.py\n\n")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
+
+
+''' 
+References
+
+1. DeepSeek-AI, “Deepseek-r1: Incentivizing reasoning capability in llms via reinforcement learning,” 2025.
+[Online]. Available: https://arxiv.org/abs/2501.12948
+
+2. Y. Li, F. Wei, C. Zhang, and H. Zhang, “EAGLE: Speculative sampling requires rethinking feature
+uncertainty,” in Proceedings of the 41st International Conference on Machine Learning, ser. Proceedings
+of Machine Learning Research, R. Salakhutdinov, Z. Kolter, K. Heller, A. Weller, N. Oliver, J. Scarlett,
+and F. Berkenkamp, Eds., vol. 235. PMLR, 21–27 Jul 2024, pp. 28 935–28 948. [Online]. Available:
+https://proceedings.mlr.press/v235/li24bt.html
+
+3. Y. Li, F. Wei, C. Zhang, and H. Zhang, “EAGLE-2: Faster inference of language models with dynamic
+draft trees,” in Proceedings of the 2024 Conference on Empirical Methods in Natural Language Processing,
+Y. Al-Onaizan, M. Bansal, and Y.-N. Chen, Eds. Miami, Florida, USA: Association for Computational
+Linguistics, Nov. 2024, pp. 7421–7432. [Online]. Available: https://aclanthology.org/2024.emnlp-main.422/
+
+4. Y. Li, F. Wei, C. Zhang, and H. Zhang, “Eagle-3: Scaling up inference acceleration of large language models
+via training-time test,” 2025. [Online]. Available: https://arxiv.org/abs/2503.01840
+
+5. C. W. F. Y. S. S. Y. W. Y. Z. Y. H. H. Z. Y. Z. Shenggui Li, Yikai Zhu, “Specforge: Train speculative decoding
+models effortlessly,” https://github.com/sgl-project/specforge, 2025.
+
+'''
