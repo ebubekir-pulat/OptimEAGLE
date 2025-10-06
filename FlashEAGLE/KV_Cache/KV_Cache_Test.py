69b9f8e261302a3ff399b3e3cc11ffff61e87f3a
Ebubekir <ebubekirpulat15@gmail.com>
2025-10-06 18:36:45 +1030

    Update KV Cache Test

--- FlashEAGLE/KV_Cache/KV_Cache_Test.py
+++ FlashEAGLE/KV_Cache/KV_Cache_Test.py
@@ -1,8 +1,6 @@
 # KV Cache Experimentation
 # Hyperparameters are "test_runs", "max_new_tokens", "temp"
 
-print("\n\n*******************************\nStarting KV_Cache_Test.py\n\n")
-
 import time
 import numpy as np
 import torch
@@ -11,12 +9,16 @@ from eagle.model.ea_model import EaModel
 from fastchat.model import get_conversation_template
 import Data
 import hashlib
-import Compress
+import sys
 
-base_model_paths = ["deepseek-ai/DeepSeek-R1-Distill-Llama-8B"]
-EAGLE_model_paths = ["yuhuili/EAGLE3-DeepSeek-R1-Distill-LLaMA-8B"]
+def main(test_id):
+    print("\n\n*******************************\nStarting KV_Cache_Test.py\n\n")
 
-def model_init():
+    base_model_paths = ["deepseek-ai/DeepSeek-R1-Distill-Llama-8B"]
+    EAGLE_model_paths = ["yuhuili/EAGLE3-DeepSeek-R1-Distill-LLaMA-8B"]
+
+    prompts = Data.specbench()
+
     # Below Code Block From: https://github.com/SafeAILab/EAGLE
     model = EaModel.from_pretrained(
         base_model_path=base_model_paths[0],
@@ -26,100 +28,91 @@ def model_init():
         attn_implementation="flash_attention_2",
         trust_remote_code=True
     )
-    # Below Code Line From: https://github.com/SafeAILab/EAGLE
     model.eval()
-    return model
 
+    KVTest_outputs = []
+    # Hyperparameters
+    test_runs = 3
+    max_new_tokens = 1024
+    temp = 0.0
 
-aai_ds = Data.aai_dataset()
-AAI_outputs = []
+    print("\nEvaluation Settings Chosen:")
+    print("Test Runs: ", test_runs)
+    print("Max New Tokens: ", max_new_tokens)
+    print("Temperature: ", temp, "\n")
 
-# Hyperparameters
-test_runs = 3
-max_new_tokens = 512
-temp = 0.0
+    # KV Cache Test Assessment Loop
+    wall_times = []
+    token_rates = []
+    avg_accept_lens = []
 
-print("\nEvaluation Settings Chosen:")
-print("Test Runs: ", test_runs)
-print("Max New Tokens: ", max_new_tokens)
-print("Temperature: ", temp)
-print("Translate: ", translate, "\n")
+    for test_run in range(test_runs):
+        run = 1
+        for i in range(len(prompts)):
+            print("Test Run: ", test_run)
+            print("Test Question: ", run)
+            run += 1
 
-# AAI Dataset Assessment Loop
-wall_times = []
-token_rates = []
-avg_accept_lens = []
-model = model_init()
+            sb_prompt = prompts[i][0]
 
-for test_run in range(test_runs):
-    run = 1
-    for question in aai_ds:
-        print("Test Run: ", test_run)
-        print("Test Question: ", run)
-        run += 1
+            # Below Code Block From: https://github.com/SafeAILab/EAGLE
+            conv = get_conversation_template(base_model_paths[0])
+            conv.append_message(conv.roles[0], sb_prompt)
+            conv.append_message(conv.roles[1], None)
+            prompt = conv.get_prompt()
+            input_ids = model.tokenizer([prompt]).input_ids
+            input_ids = torch.as_tensor(input_ids).cuda()
 
-        # Below Code Block From: https://github.com/SafeAILab/EAGLE
-        your_message = question
-        if translate == True:
-            your_message = Compress.zh_to_en(your_message)[0]
-        conv = get_conversation_template(base_model_paths[0])
-        conv.append_message(conv.roles[0], your_message)
-        conv.append_message(conv.roles[1], None)
-        prompt = conv.get_prompt()
-        input_ids = model.tokenizer([prompt]).input_ids
-        input_ids = torch.as_tensor(input_ids).cuda()
+            start = time.perf_counter_ns()
 
-        start = time.perf_counter_ns()
-
-        # Below Code Line From: https://github.com/SafeAILab/EAGLE
-        output_ids = model.eagenerate(input_ids, temperature=temp, max_new_tokens=max_new_tokens, log=True)
-
-        finish = time.perf_counter_ns()
-
-        if translate == True:
             # Below Code Line From: https://github.com/SafeAILab/EAGLE
-            aai_output = Compress.en_to_zh(model.tokenizer.decode(output_ids[0][0]))[0]
-        else:
+            output_ids = model.eagenerate(input_ids, temperature=temp, max_new_tokens=max_new_tokens, log=True)
+
+            finish = time.perf_counter_ns()
+
             # Below Code Line From: https://github.com/SafeAILab/EAGLE
-            aai_output = model.tokenizer.decode(output_ids[0][0])
+            kv_output = model.tokenizer.decode(output_ids[0][0])
 
-        elapsed = finish - start
-        wall_times.append(elapsed)
+            elapsed = finish - start
+            wall_times.append(elapsed)
 
-        new_tokens = int(output_ids[1])
-        tokens_per_second = new_tokens / (elapsed * pow(10, -9))
-        token_rates.append(tokens_per_second)
+            new_tokens = int(output_ids[1])
+            tokens_per_second = new_tokens / (elapsed * pow(10, -9))
+            token_rates.append(tokens_per_second)
 
-        # Reference for below code block: https://github.com/SafeAILab/EAGLE/issues/153
-        steps = int(output_ids[2])
-        avg_accept_len = new_tokens / steps
-        avg_accept_lens.append(avg_accept_len)
+            # Reference for below code block: https://github.com/SafeAILab/EAGLE/issues/153
+            steps = int(output_ids[2])
+            avg_accept_len = new_tokens / steps
+            avg_accept_lens.append(avg_accept_len)
 
-        # Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
-        output = {
-            "id": hashlib.md5((str(test_run) + question + Data.extract_response(aai_output)).encode()).hexdigest(),
-            "output": Data.extract_response(aai_output)
-        }
-        AAI_outputs.append(output)
+            # Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
+            output = {
+                "id": hashlib.md5((str(test_run) + sb_prompt + Data.extract_response(kv_output)).encode()).hexdigest(),
+                "output": Data.extract_response(kv_output)
+            }
+            KVTest_outputs.append(output)
 
+    # Print KV Cache Test Results
+    print(f"KV Cache Test Results for {EAGLE_model_paths[0]}:")
+    print("Base Model: ", base_model_paths[0])
+    print("Mean Wall Time (ns): ", np.mean(wall_times))
+    print("Mean Tokens Generated/s: ", np.mean(token_rates))
+    print("Average Acceptance Length: ", np.mean(avg_accept_lens))
 
-# Print AAI Dataset Results
-print(f"AAI Results for {EAGLE_model_paths[0]}:")
-print("Mean Wall Time (ns): ", np.mean(wall_times))
-print("Mean Tokens Generated/s: ", np.mean(token_rates))
-print("Average Acceptance Length: ", np.mean(avg_accept_lens))
+    # Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
+    with open(f"KV_Cache_Test_Output_{EAGLE_model_paths[0].replace('/', '-')}_{test_id}.jsonl", "x") as f:
+        for output in KVTest_outputs:
+            f.write(json.dumps(output) + "\n")
 
-translate_tag = ""
+    print("\n\nOutput Data: \n")
 
-if translate == True:
-    translate_tag = "_Translate"
+    for output in KVTest_outputs:
+        print(output)
 
-# Below Code Block From: https://github.com/sgl-project/SpecForge/blob/main/scripts/prepare_data.py
-with open(f"AAI_Output_EAGLE3{translate_tag}_{EAGLE_model_paths[0].replace("/", "-")}.jsonl", "x") as f:
-    for output in AAI_outputs:
-        f.write(json.dumps(output) + "\n")
+    print("\n\n*******************************\nFinished Running KV_Cache_Test.py\n\n")
 
-print("\n\n*******************************\nFinished Running AAI_EAGLE3_Test.py\n\n")
+if __name__ == "__main__":
+    main(sys.argv[1])
 
 '''
 References
