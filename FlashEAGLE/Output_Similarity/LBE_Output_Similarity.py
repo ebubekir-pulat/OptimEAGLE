7a9e3febacc324459896eb22793eb346753691bc
Ebubekir <ebubekirpulat15@gmail.com>
2025-10-22 23:42:44 +1030

    Add LBE output sim

---
+++ FlashEAGLE/Output_Similarity/LBE_Output_Similarity.py
@@ -0,0 +1,66 @@
+print("\n\n*******************************\nStarting LBE_Output_Similarity.py\n\n")
+
+from FlagEmbedding import BGEM3FlagModel
+import numpy as np
+
+# Code in main() From: https://huggingface.co/BAAI/bge-m3
+def main():
+    model = BGEM3FlagModel('BAAI/bge-m3',  
+                        use_fp16=True)
+    
+    # Comparing Autoregressive Decoding Outputs
+
+    # Note: Copy Experimental Output Into Corresponding Empty Arrays
+    autoreg_standard = []
+    autoreg_summ = []
+    autoreg_skip = []
+    autoreg_ranked = []
+    autoreg_similarity_scores = []
+    
+    for i in range(len(autoreg_notrans)):
+        embeddings_1 = model.encode(autoreg_notrans[i])['dense_vecs']
+        embeddings_2 = model.encode(autoreg_trans[i])['dense_vecs']
+        similarity = embeddings_1 @ embeddings_2.T
+        autoreg_similarity_scores.append(similarity)
+
+    print("\nMean Similarity Score Between AutoReg-NoTranslate & AutoReg-Translate: ", np.mean(autoreg_similarity_scores))
+
+    # Comparing EAGLE-3 Outputs
+
+    # Note: Copy Experimental Output Into Corresponding Empty Arrays
+    eagle3_standard = []
+    eagle3_summ = []
+    eagle3_skip = []
+    eagle3_ranked = []
+    
+    # Output Quality of EAGLE-SUMM
+    eagle3_similarity_scores = []
+    
+    for i in range(len(eagle3_standard)):
+        embeddings_1 = model.encode(eagle3_standard[i])['dense_vecs']
+        embeddings_2 = model.encode(eagle3_summ[i])['dense_vecs']
+        similarity = embeddings_1 @ embeddings_2.T
+        eagle3_similarity_scores.append(similarity)
+
+    print("\nMean Similarity Score Between EAGLE3-NoTranslate & EAGLE3-Translate: ", np.mean(eagle3_similarity_scores))
+
+    print("\n\n*******************************\nFinished Running AAI_Output_Similarity.py\n\n")
+
+if __name__ == '__main__':
+    main()
+
+'''
+References
+
+1. J. Chen, S. Xiao, P. Zhang, K. Luo, D. Lian, and Z. Liu, “Bge m3-embedding: Multi-lingual, multi-functionality,
+multi-granularity text embeddings through self-knowledge distillation,” 2023.
+
+2. S. Xiao, Z. Liu, P. Zhang, and N. Muennighoff, “C-pack: Packaged resources to advance general chinese embed-
+ding,” 2023.
+
+3. S. Xiao, Z. Liu, P. Zhang, and X. Xing, “Lm-cocktail: Resilient tuning of language models via model merging,”
+2023.
+
+4. P. Zhang, S. Xiao, Z. Liu, Z. Dou, and J.-Y. Nie, “Retrieve anything to augment large language models,” 2023.
+
+'''
