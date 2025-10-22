d61617acbd5e30c7181c20d533f88eb56189e7ff
Ebubekir <ebubekirpulat15@gmail.com>
2025-10-22 21:56:39 +1030

    Output_Sim

--- FlashEAGLE/Utilities/Output_Similarity.py
+++ FlashEAGLE/Utilities/Output_Similarity.py
@@ -1,36 +1,28 @@
 print("\n\n*******************************\nStarting Output_Similarity.py\n\n")
 
-# pip install -U FlagEmbedding
-
 from FlagEmbedding import BGEM3FlagModel
 import numpy as np
-import pandas as pd
-import sys
 
 # Below Code From: https://huggingface.co/BAAI/bge-m3
-def main(file1, file2):
-    first_answers = pd.read_json(file1, lines=True)
-    second_answers = pd.read_json(file2, lines=True)
+def main():
+    first_answers = ["My name is Bob.", "Where is the Chicken?"]
+    second_answers = ["My name is Bob.", "My name is Bob."]
     similarity_scores = []
 
     model = BGEM3FlagModel('BAAI/bge-m3',  
                         use_fp16=True)
     
     for i in range(len(first_answers)):
-        embeddings_1 = model.encode(first_answers[i]["output"])['dense_vecs']
-        embeddings_2 = model.encode(second_answers[i]["output"])['dense_vecs']
+        embeddings_1 = model.encode(first_answers[i])['dense_vecs']
+        embeddings_2 = model.encode(second_answers[i])['dense_vecs']
         similarity = embeddings_1 @ embeddings_2.T
         similarity_scores.append(similarity)
 
-    print(f"File 1: {file1}")
-    print(f"File 2: {file2}")
     print("Mean Similarity Score: ", np.mean(similarity_scores))
+    print("\n\n*******************************\nFinished Running Output_Similarity.py\n\n")
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
-
-
-print("\n\n*******************************\nFinished Running Output_Similarity.py\n\n")
+    main()
 
 '''
 References
